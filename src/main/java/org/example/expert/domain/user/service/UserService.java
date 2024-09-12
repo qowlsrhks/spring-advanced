ea7c2ac03246b6d43a26acab07d49a8615152cfd
qowlsrhks <skswlsrhks@naver.com>
2024-09-12 14:39:32 +0900

    레벨 1-3: 메서드 분리

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -24,24 +24,28 @@ public class UserService {
     }
 
     @Transactional
-    public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
+    public void changePassword(Long userId, UserChangePasswordRequest userChangePasswordRequest) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new InvalidRequestException("User not found"));
 
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new InvalidRequestException("User not found"));
+//        유효성 검증
+        validatePassword(userChangePasswordRequest);
 
-        if (passwordEncoder.matches(userChangePasswordRequest.getNewPassword(), user.getPassword())) {
+        if (passwordEncoder.matches(userChangePasswordRequest.getNewPassword(), user.getPassword())){
             throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
         }
 
         if (!passwordEncoder.matches(userChangePasswordRequest.getOldPassword(), user.getPassword())) {
             throw new InvalidRequestException("잘못된 비밀번호입니다.");
         }
+       user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
+    }
 
-        user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
+//    메소드 분리
+    public void validatePassword(UserChangePasswordRequest userChangePasswordRequest) {
+        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
+                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
+                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
+            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
+        }
     }
 }
